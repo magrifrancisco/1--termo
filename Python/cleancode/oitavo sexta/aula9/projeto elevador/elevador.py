1edc4948eb9878b1ec399a9a168a6d99c923d7d3
Francisco Miguel <mimagri.francisco09@gmail.com>
2026-05-08 11:23:58 -0300

    Sistema de elevador

---
+++ Python/cleancode/oitavo sexta/aula9/projeto elevador/elevador.py
@@ -0,0 +1,18 @@
+# Sistema de Elevador de prédio
+# O prédio possui 10 andares, sendo o térreo o andar 0. O elevador pode se mover para cima ou para baixo, e tem a capacidade de transportar até 5 pessoas.
+# O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar. 
+# O elevador deve deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa.
+# O elevador deve exibir uma mensagem indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo, descendo, parando). O programa deve continuar rodando até que o usuário decida encerrar.
+ 
+# Levantamento de requisitos 
+# Requisitos Funcionais(RF):
+# O elevador pode se mover para cima e para baixo 
+# O elevador pode ser chamado por qualquer pessoa em qualquer andar do prédio
+# O elevador tem a capacidade de transportar até 5 pessoas
+# O elevador deve ir até o andar em que a pessoa chamou e depois ir até o destino
+
+
+# Requisitos Não Funcionais(RNF)
+# O elevador deve exibir uma mensagem indicando o andar atual da pessoa 
+# O programa deve continuar funcionando e rodando até que a pessoa que está no elevador decida parar
+# O elevador começa no andar 0 
