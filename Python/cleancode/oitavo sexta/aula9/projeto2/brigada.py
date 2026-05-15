6c2ade76a6b461c0462c63f4a5cb760ba67727c9
Francisco Miguel <mimagri.francisco09@gmail.com>
2026-05-15 11:25:39 -0300

    brigada atividade

---
+++ Python/cleancode/oitavo sexta/aula9/projeto2/brigada.py
@@ -0,0 +1,28 @@
+# # # 1. Cadastro de Funcionários:
+# # # ○ Armazene o nome, setor e o status dos treinamentos (NR-10, NR-35 e
+# # # Brigada).
+
+# # # 2. Verificação de EPI (NR-6):
+# # # ○ O sistema deve receber o setor do funcionário.
+# # # ○ Se o setor for "Elétrica", liste a obrigatoriedade de luvas de alta tensão e
+# # # botas dielétricas.
+# # # ○ Se o setor for "Trabalho em Altura", liste o cinturão de segurança e
+# # # # talabarte.
+
+# # 3. Alerta de Reciclagem:
+# # ○ Crie uma função que receba o ano do último treinamento da Brigada de
+# # Incêndio.
+# # ○ Se o treinamento tiver mais de 2 anos, exiba a mensagem: "Treinamento
+# # Vencido! Encaminhar para reciclagem."
+# # ○ Caso contrário, exiba: "Treinamento Válido."
+
+# 4. Relatório Geral:
+# ○ Exiba na tela um resumo com o total de funcionários cadastrados e quantos
+# estão com treinamentos em dia.
+
+# Requisitos Funcionais (RF)
+# O sistema deve armazenar o nome, setor e o status dos treinamentos de todos os funcionários
+# Se o Setor for "elétrica", liste a obrigatoriedade de luvas de alta tensão e botas dielétricas
+# Se o setor for de trabalho em altura, liste o cinturão de segurança e tabalarte 
+
+# Requisitos Não Funcionais 
